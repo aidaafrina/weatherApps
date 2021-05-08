f1b5fa611e86b0b49211daec44a676b850ab10c0
aidaafrina <aidaafrina90.aa@gmail.com>
2021-05-08 14:07:53 +0800

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Weather from './weather/Weather';
+import { API_KEY } from './utils/WeatherApiKey';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+    state = {
+        isLoading: true,
+        temperature: 0,
+        weatherConditions: null,
+        error: null
+    };
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                this.fetchWeather(position.coords.latitude, position.coords.longitude);
+            },
+            error => {
+                this.setState({
+                    error: 'Error'
+                });
+            }
+        );
+    }
+
+    fetchWeather(lat = 25, lon = 25) {
+        fetch(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+        )
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    temperature: json.main.temp,
+                    weatherCondition: json.weather[0].main,
+                    isLoading: false
+                });
+            });
+    }
+
+    render() {
+        const { isLoading } = this.state;
+        return (
+            <View style={styles.container}>
+        <Text style={styles.name}>NUR AIDA AFRINA BINTI ZULNAZRI (1729832)</Text><br></br>
+                {isLoading ? (
+                    <Text style={styles.loading}>Fetching The Weather</Text>
+                ) : (
+                    <Weather
+                        weather={this.state.weatherCondition}
+                        temperature={this.state.temperature}
+                    />
+                )}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1
+    },
+
+    name: {
+        alignContent: 'center',
+        color: "black",
+        fontSize: 15,
+        fontWeight: "bold",
+        textAlign: "center",
+        backgroundColor: "#9FE2BF"
+    },
+
+    loading: {
+        alignContent: 'center',
+        color: "white",
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+        backgroundColor: "#add8e6"
+    }
+
+   
+    
 });
